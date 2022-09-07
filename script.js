0edac3e0ee11647a21407db29f736809f250158f
SkellyBG <skellybg245@gmail.com>
2022-09-08 01:01:23 +1000

    Add game function

--- script.js
+++ script.js
@@ -33,3 +33,10 @@ function playRound(playerSelection, computerSelection) {
 // const playerSelection = "rock";
 // const computerSelection = getComputerChoice();
 // console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let input = prompt("Enter your choice: ");
+        console.log(playRound(input, getComputerChoice()));
+    }
+}
