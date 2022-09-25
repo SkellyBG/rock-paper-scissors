f3b7601f1ff45768f1c580b56eb50ca4ade8f0cf
SkellyBG <skellybg245@gmail.com>
2022-09-25 14:51:11 +1000

    Display final result once a player reach 5 points

--- script.js
+++ script.js
@@ -19,15 +19,17 @@ function playRound(playerSelection, computerSelection) {
   ]
   const choices = ["Rock", "Paper", "Scissor"];
   result = table[choices.indexOf(playerSelection)][choices.indexOf(computerSelection)];
-  
+  return result;
+}
+
+function formatResult(result, playerSelection, computerSelection) {
   if (result === "Win") {
-      return "You Win! " + playerSelection + " beats " + computerSelection; 
+    return "You Win! " + playerSelection + " beats " + computerSelection; 
   } else if (result === "Draw") {
-      return "You Draw! " + playerSelection + " ties " + computerSelection;
-  } else if (result === "Lose") {
-      return "You Lose! " + playerSelection + " loses to " + computerSelection;
+    return "You Draw! " + playerSelection + " ties " + computerSelection;
+  } else {
+    return "You Lose! " + playerSelection + " loses to " + computerSelection;
   }
-  return;
 }
 
 // const playerSelection = "rock";
@@ -37,11 +39,40 @@ function playRound(playerSelection, computerSelection) {
 function game(input) {
   const resultDiv = document.querySelector("#result");
   const resultP = document.createElement("p");
-  resultP.textContent = playRound(input, getComputerChoice());
+  const computerChoice = getComputerChoice();
+  const result = playRound(input, computerChoice);
+  resultP.textContent = formatResult(result, input, computerChoice);
   if (resultDiv.firstChild) {
     resultDiv.removeChild(resultDiv.firstChild);
   }
   resultDiv.appendChild(resultP);
+
+  const scoreDiv = document.querySelector("#score");
+  if (result === "Win") {
+    playerScore++;
+  } else if (result === "Lose") {
+    computerScore++;
+  }
+  const scoreP = document.createElement("p");
+  scoreP.textContent = "YOU " + playerScore + ":" + computerScore + " COMPUTER";
+  if (scoreDiv.firstChild) {
+    scoreDiv.removeChild(scoreDiv.firstChild);
+  }
+  scoreDiv.appendChild(scoreP);
+
+  if (playerScore === 5 || computerScore === 5) {
+    const finalResultDiv = document.querySelector("#final-result");
+    const finalResultP = document.createElement("p");
+    if (playerScore === 5) {
+      finalResultP.textContent = "YOU WIN!";
+    } else {
+      finalResultP.textContent = "COMPUTER WIN!";
+    }
+    finalResultDiv.appendChild(finalResultP);
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorBtn.disabled = true;
+  }
 }
 
 const rockBtn = document.querySelector("#rock");
@@ -50,3 +81,6 @@ const paperBtn = document.querySelector("#paper");
 paperBtn.addEventListener('click', function(e) {game("Paper")});
 const scissorBtn = document.querySelector("#scissor");
 scissorBtn.addEventListener('click', function(e) {game("Scissor")});
+
+let playerScore = 0;
+let computerScore = 0;
