617ecb5c0df21925faaa0aa43dbdf81d8505566b
SkellyBG <skellybg245@gmail.com>
2022-09-25 14:21:33 +1000

    Add buttons and div for result display

--- script.js
+++ script.js
@@ -4,42 +4,49 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    let result = "";
-    //         Rock  Paper  Scissor | Player
-    //    Rock 
-    //   Paper 
-    // Scissor
-    // -------
-    // Computer
-    const table = [
-        ["Draw", "Win", "Lose"],
-        ["Lose", "Draw", "Win"],
-        ["Win", "Lose", "Draw"],
-    ]
-    const choices = ["Rock", "Paper", "Scissor"];
-    result = table[choices.indexOf(playerSelection)][choices.indexOf(computerSelection)];
-    
-    if (result === "Win") {
-        return "You Win! " + playerSelection + " beats " + computerSelection; 
-    } else if (result === "Draw") {
-        return "You Draw! " + playerSelection + " ties " + computerSelection;
-    } else if (result === "Lose") {
-        return "You Lose! " + playerSelection + " loses to " + computerSelection;
-    }
-    return;
+  playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+  let result = "";
+  //         Rock  Paper  Scissor | Player
+  //    Rock 
+  //   Paper 
+  // Scissor
+  // -------
+  // Computer
+  const table = [
+      ["Draw", "Win", "Lose"],
+      ["Lose", "Draw", "Win"],
+      ["Win", "Lose", "Draw"],
+  ]
+  const choices = ["Rock", "Paper", "Scissor"];
+  result = table[choices.indexOf(playerSelection)][choices.indexOf(computerSelection)];
+  
+  if (result === "Win") {
+      return "You Win! " + playerSelection + " beats " + computerSelection; 
+  } else if (result === "Draw") {
+      return "You Draw! " + playerSelection + " ties " + computerSelection;
+  } else if (result === "Lose") {
+      return "You Lose! " + playerSelection + " loses to " + computerSelection;
+  }
+  return;
 }
 
 // const playerSelection = "rock";
 // const computerSelection = getComputerChoice();
 // console.log(playRound(playerSelection, computerSelection));
 
-function game() {
-  let input = prompt("Enter your choice: ");
-  console.log("Round " + (i + 1) + ": " + playRound(input, getComputerChoice()));
+function game(input) {
+  const resultDiv = document.querySelector("#result");
+  const resultP = document.createElement("p");
+  resultP.textContent = playRound(input, getComputerChoice());
+  if (resultDiv.firstChild) {
+    resultDiv.removeChild(resultDiv.firstChild);
+  }
+  resultDiv.appendChild(resultP);
 }
 
 const rockBtn = document.querySelector("#rock");
+rockBtn.addEventListener('click', function(e) {game("Rock")});
 const paperBtn = document.querySelector("#paper");
+paperBtn.addEventListener('click', function(e) {game("Paper")});
 const scissorBtn = document.querySelector("#scissor");
-game();
+scissorBtn.addEventListener('click', function(e) {game("Scissor")});
